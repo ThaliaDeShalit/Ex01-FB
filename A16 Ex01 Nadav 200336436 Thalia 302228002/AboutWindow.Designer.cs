b85568ca10fb39181a1b3527e21cfc23158367b7
ThaliaDeShalit <thaliapenguin@gmail.com>
2015-11-19 18:12:52 +0200

    tried helping with the about winow design

--- A16 Ex01 Nadav 200336436 Thalia 302228002/AboutWindow.Designer.cs
+++ A16 Ex01 Nadav 200336436 Thalia 302228002/AboutWindow.Designer.cs
@@ -28,101 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label_UserAbout = new System.Windows.Forms.Label();
-            this.label_Hometown = new System.Windows.Forms.Label();
-            this.label_Birthday = new System.Windows.Forms.Label();
-            this.label_Education = new System.Windows.Forms.Label();
-            this.label_UserBirthday = new System.Windows.Forms.Label();
-            this.label_UserHometown = new System.Windows.Forms.Label();
-            this.label_UserEducation = new System.Windows.Forms.Label();
-            this.label_About = new System.Windows.Forms.Label();
+            this.labelUserAbout = new System.Windows.Forms.Label();
+            this.labelHometown = new System.Windows.Forms.Label();
+            this.labelBirthday = new System.Windows.Forms.Label();
+            this.labelEducation = new System.Windows.Forms.Label();
+            this.labelUserBirthday = new System.Windows.Forms.Label();
+            this.labelUserHometown = new System.Windows.Forms.Label();
+            this.labelUserEducation = new System.Windows.Forms.Label();
+            this.labelAbout = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label_UserAbout
+            // labelUserAbout
             // 
-            this.label_UserAbout.AutoSize = true;
-            this.label_UserAbout.Location = new System.Drawing.Point(84, 59);
-            this.label_UserAbout.Name = "label_UserAbout";
-            this.label_UserAbout.Size = new System.Drawing.Size(75, 17);
-            this.label_UserAbout.TabIndex = 0;
-            this.label_UserAbout.Text = "AboutUser";
+            this.labelUserAbout.AutoSize = true;
+            this.labelUserAbout.BackColor = System.Drawing.Color.Transparent;
+            this.labelUserAbout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelUserAbout.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.labelUserAbout.Location = new System.Drawing.Point(84, 59);
+            this.labelUserAbout.Name = "labelUserAbout";
+            this.labelUserAbout.Size = new System.Drawing.Size(157, 33);
+            this.labelUserAbout.TabIndex = 0;
+            this.labelUserAbout.Text = "AboutUser";
             // 
-            // label_Hometown
+            // labelHometown
             // 
-            this.label_Hometown.AutoSize = true;
-            this.label_Hometown.Location = new System.Drawing.Point(36, 170);
-            this.label_Hometown.Name = "label_Hometown";
-            this.label_Hometown.Size = new System.Drawing.Size(78, 17);
-            this.label_Hometown.TabIndex = 1;
-            this.label_Hometown.Text = "Hometown:";
+            this.labelHometown.AutoSize = true;
+            this.labelHometown.BackColor = System.Drawing.Color.Transparent;
+            this.labelHometown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelHometown.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.labelHometown.Location = new System.Drawing.Point(12, 171);
+            this.labelHometown.Name = "labelHometown";
+            this.labelHometown.Size = new System.Drawing.Size(174, 33);
+            this.labelHometown.TabIndex = 1;
+            this.labelHometown.Text = "Hometown:";
             // 
-            // label_Birthday
+            // labelBirthday
             // 
-            this.label_Birthday.AutoSize = true;
-            this.label_Birthday.Location = new System.Drawing.Point(36, 136);
-            this.label_Birthday.Name = "label_Birthday";
-            this.label_Birthday.Size = new System.Drawing.Size(64, 17);
-            this.label_Birthday.TabIndex = 2;
-            this.label_Birthday.Text = "Birthday:";
+            this.labelBirthday.AutoSize = true;
+            this.labelBirthday.BackColor = System.Drawing.Color.Transparent;
+            this.labelBirthday.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelBirthday.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.labelBirthday.Location = new System.Drawing.Point(12, 136);
+            this.labelBirthday.Name = "labelBirthday";
+            this.labelBirthday.Size = new System.Drawing.Size(141, 33);
+            this.labelBirthday.TabIndex = 2;
+            this.labelBirthday.Text = "Birthday:";
             // 
-            // label_Education
+            // labelEducation
             // 
-            this.label_Education.AutoSize = true;
-            this.label_Education.Location = new System.Drawing.Point(36, 204);
-            this.label_Education.Name = "label_Education";
-            this.label_Education.Size = new System.Drawing.Size(75, 17);
-            this.label_Education.TabIndex = 4;
-            this.label_Education.Text = "Education:";
+            this.labelEducation.AutoSize = true;
+            this.labelEducation.BackColor = System.Drawing.Color.Transparent;
+            this.labelEducation.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelEducation.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.labelEducation.Location = new System.Drawing.Point(12, 204);
+            this.labelEducation.Name = "labelEducation";
+            this.labelEducation.Size = new System.Drawing.Size(159, 33);
+            this.labelEducation.TabIndex = 4;
+            this.labelEducation.Text = "Education:";
             // 
-            // label_UserBirthday
+            // labelUserBirthday
             // 
-            this.label_UserBirthday.AutoSize = true;
-            this.label_UserBirthday.Location = new System.Drawing.Point(130, 136);
-            this.label_UserBirthday.Name = "label_UserBirthday";
-            this.label_UserBirthday.Size = new System.Drawing.Size(90, 17);
-            this.label_UserBirthday.TabIndex = 5;
-            this.label_UserBirthday.Text = "UserBirthday";
+            this.labelUserBirthday.AutoSize = true;
+            this.labelUserBirthday.BackColor = System.Drawing.Color.Transparent;
+            this.labelUserBirthday.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelUserBirthday.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.labelUserBirthday.Location = new System.Drawing.Point(178, 136);
+            this.labelUserBirthday.Name = "labelUserBirthday";
+            this.labelUserBirthday.Size = new System.Drawing.Size(193, 33);
+            this.labelUserBirthday.TabIndex = 5;
+            this.labelUserBirthday.Text = "UserBirthday";
             // 
-            // label_UserHometown
+            // labelUserHometown
             // 
-            this.label_UserHometown.AutoSize = true;
-            this.label_UserHometown.Location = new System.Drawing.Point(130, 170);
-            this.label_UserHometown.Name = "label_UserHometown";
-            this.label_UserHometown.Size = new System.Drawing.Size(104, 17);
-            this.label_UserHometown.TabIndex = 6;
-            this.label_UserHometown.Text = "UserHometown";
+            this.labelUserHometown.AutoSize = true;
+            this.labelUserHometown.BackColor = System.Drawing.Color.Transparent;
+            this.labelUserHometown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelUserHometown.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.labelUserHometown.Location = new System.Drawing.Point(178, 169);
+            this.labelUserHometown.Name = "labelUserHometown";
+            this.labelUserHometown.Size = new System.Drawing.Size(226, 33);
+            this.labelUserHometown.TabIndex = 6;
+            this.labelUserHometown.Text = "UserHometown";
             // 
-            // label_UserEducation
+            // labelUserEducation
             // 
-            this.label_UserEducation.AutoSize = true;
-            this.label_UserEducation.Location = new System.Drawing.Point(130, 204);
-            this.label_UserEducation.Name = "label_UserEducation";
-            this.label_UserEducation.Size = new System.Drawing.Size(101, 17);
-            this.label_UserEducation.TabIndex = 7;
-            this.label_UserEducation.Text = "UserEducation";
+            this.labelUserEducation.AutoSize = true;
+            this.labelUserEducation.BackColor = System.Drawing.Color.Transparent;
+            this.labelUserEducation.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelUserEducation.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.labelUserEducation.Location = new System.Drawing.Point(178, 202);
+            this.labelUserEducation.Name = "labelUserEducation";
+            this.labelUserEducation.Size = new System.Drawing.Size(211, 33);
+            this.labelUserEducation.TabIndex = 7;
+            this.labelUserEducation.Text = "UserEducation";
             // 
-            // label_About
+            // labelAbout
             // 
-            this.label_About.AutoSize = true;
-            this.label_About.Location = new System.Drawing.Point(84, 20);
-            this.label_About.Name = "label_About";
-            this.label_About.Size = new System.Drawing.Size(32, 17);
-            this.label_About.TabIndex = 8;
-            this.label_About.Text = "Bio:";
+            this.labelAbout.AutoSize = true;
+            this.labelAbout.BackColor = System.Drawing.Color.Transparent;
+            this.labelAbout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelAbout.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.labelAbout.Location = new System.Drawing.Point(84, 20);
+            this.labelAbout.Name = "labelAbout";
+            this.labelAbout.Size = new System.Drawing.Size(69, 33);
+            this.labelAbout.TabIndex = 8;
+            this.labelAbout.Text = "Bio:";
             // 
             // AboutWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(262, 288);
-            this.Controls.Add(this.label_About);
-            this.Controls.Add(this.label_UserEducation);
-            this.Controls.Add(this.label_UserHometown);
-            this.Controls.Add(this.label_UserBirthday);
-            this.Controls.Add(this.label_Education);
-            this.Controls.Add(this.label_Birthday);
-            this.Controls.Add(this.label_Hometown);
-            this.Controls.Add(this.label_UserAbout);
+            this.ClientSize = new System.Drawing.Size(496, 303);
+            this.Controls.Add(this.labelAbout);
+            this.Controls.Add(this.labelUserEducation);
+            this.Controls.Add(this.labelUserHometown);
+            this.Controls.Add(this.labelUserBirthday);
+            this.Controls.Add(this.labelEducation);
+            this.Controls.Add(this.labelBirthday);
+            this.Controls.Add(this.labelHometown);
+            this.Controls.Add(this.labelUserAbout);
             this.Name = "AboutWindow";
             this.Text = "AboutWindow";
             this.ResumeLayout(false);
@@ -132,13 +156,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label_UserAbout;
-        private System.Windows.Forms.Label label_Hometown;
-        private System.Windows.Forms.Label label_Birthday;
-        private System.Windows.Forms.Label label_Education;
-        private System.Windows.Forms.Label label_UserBirthday;
-        private System.Windows.Forms.Label label_UserHometown;
-        private System.Windows.Forms.Label label_UserEducation;
-        private System.Windows.Forms.Label label_About;
+        private System.Windows.Forms.Label labelUserAbout;
+        private System.Windows.Forms.Label labelHometown;
+        private System.Windows.Forms.Label labelBirthday;
+        private System.Windows.Forms.Label labelEducation;
+        private System.Windows.Forms.Label labelUserBirthday;
+        private System.Windows.Forms.Label labelUserHometown;
+        private System.Windows.Forms.Label labelUserEducation;
+        private System.Windows.Forms.Label labelAbout;
     }
 }
